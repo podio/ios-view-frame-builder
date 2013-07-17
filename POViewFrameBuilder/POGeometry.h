752ec80bc060fa6357cf49e97750c7bebe109a3a
Sebastian Rehnby <sebastian@rehnby.com>
2013-07-17 12:21:09 +0200

    Move inline geometry functions to header file

--- POViewFrameBuilder/POGeometry.h
+++ POViewFrameBuilder/POGeometry.h
@@ -5,18 +5,65 @@
 //  Copyright (c) 2012 Citrix Systems, Inc. All rights reserved.
 //
 
-extern inline CGRect PORectInsets(CGRect rect, CGFloat top, CGFloat left, CGFloat bottom, CGFloat right);
+#import <UIKit/UIKit.h>
 
-extern inline CGRect PORectFromSize(CGFloat width, CGFloat height);
-extern inline CGRect PORectWithSize(CGRect rect, CGFloat width, CGFloat height);
-extern inline CGRect PORectWithWidth(CGRect rect, CGFloat width);
-extern inline CGRect PORectWithHeight(CGRect rect, CGFloat height);
+static inline CGRect PORectInsets(CGRect rect, CGFloat top, CGFloat left, CGFloat bottom, CGFloat right) {
+  return UIEdgeInsetsInsetRect(rect, UIEdgeInsetsMake(top, left, bottom, right));
+}
 
-extern inline CGRect PORectWithOrigin(CGRect rect, CGFloat x, CGFloat y);
-extern inline CGRect PORectWithX(CGRect rect, CGFloat x);
-extern inline CGRect PORectWithY(CGRect rect, CGFloat y);
+static inline CGRect PORectWithSize(CGRect rect, CGFloat width, CGFloat height) {
+  rect.size.width = width;
+  rect.size.height = height;
+  
+  return rect;
+}
 
-extern inline CGPoint POPointWithOffset(CGPoint p, CGFloat dx, CGFloat dy);
-extern inline CGPoint POPointCenterInSize(CGSize s);
+static inline CGRect PORectFromSize(CGFloat width, CGFloat height) {
+  return PORectWithSize(CGRectZero, width, height);
+}
 
-extern inline CGRect PORectMove(CGRect rect, CGFloat dx, CGFloat dy);
+static inline CGRect PORectWithWidth(CGRect rect, CGFloat width) {
+  rect.size.width = width;
+  
+  return rect;
+}
+
+static inline CGRect PORectWithHeight(CGRect rect, CGFloat height) {
+  rect.size.height = height;
+  
+  return rect;
+}
+
+static inline CGRect PORectWithOrigin(CGRect rect, CGFloat x, CGFloat y) {
+  rect.origin.x = x;
+  rect.origin.y = y;
+  
+  return rect;
+}
+
+static inline CGRect PORectWithX(CGRect rect, CGFloat x) {
+  rect.origin.x = x;
+  
+  return rect;
+}
+
+static inline CGRect PORectWithY(CGRect rect, CGFloat y) {
+  rect.origin.y = y;
+  
+  return rect;
+}
+
+static inline CGPoint POPointWithOffset(CGPoint p, CGFloat dx, CGFloat dy) {
+  return CGPointMake(p.x + dx, p.y + dy);
+}
+
+static inline CGPoint POPointCenterInSize(CGSize s) {
+  return CGPointMake(roundf(s.width / 2), roundf(s.height / 2));
+}
+
+static inline CGRect PORectMove(CGRect rect, CGFloat dx, CGFloat dy) {
+  rect.origin.x += dx;
+  rect.origin.y += dy;
+  
+  return rect;
+}
