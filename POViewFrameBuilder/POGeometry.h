a6459395d761cb5fea2ed168fdc1f2fa553595e8
Romain Briche <romain.briche@gmail.com>
2013-08-05 09:50:52 +0200

    Add a few more macros

--- POViewFrameBuilder/POGeometry.h
+++ POViewFrameBuilder/POGeometry.h
@@ -61,6 +61,13 @@ static inline CGPoint POPointCenterInSize(CGSize s) {
   return CGPointMake(roundf(s.width / 2), roundf(s.height / 2));
 }
 
+static inline CGPoint PORectCenter(CGRect rect) {
+  return (CGPoint){
+    .x = CGRectGetMidX(rect),
+    .y = CGRectGetMidY(rect)
+  };
+}
+
 static inline CGRect PORectMove(CGRect rect, CGFloat dx, CGFloat dy) {
   rect.origin.x += dx;
   rect.origin.y += dy;
@@ -72,4 +79,12 @@ static inline CGSize POEdgeInsetsInsetSize(CGSize size, UIEdgeInsets insets) {
   size.width  -= (insets.left + insets.right);
   size.height -= (insets.top  + insets.bottom);
   return size;
+}
+
+static inline UIEdgeInsets POEdgeInsetsUnion(UIEdgeInsets insets1, UIEdgeInsets insets2) {
+  insets1.top    += insets2.top;
+  insets1.left   += insets2.left;
+  insets1.bottom += insets2.bottom;
+  insets1.right  += insets2.right;
+  return insets1;
 }
